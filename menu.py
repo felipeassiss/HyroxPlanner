0b8756b22b46ca395864afb745162af8bbd7cee1
felipe <felipeassisfsantos@gmail.com>
2026-04-10 22:03:32 -0300

    feat: criacao menu

---
+++ menu.py
@@ -0,0 +1,4 @@
+def menu():
+    print("\t \t MENU HYROX Planner \t \t \t")
+    opcao = input(("Escolha uma opção:\n \n1-adicionar treino \n2-visualizar treinos existentes \n3-editar treinos \n4-excluir treinos \n\n"))
+menu()
